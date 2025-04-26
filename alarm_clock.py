84fd2e1275d2c28e8e6dca4b3a9f81748fc3a80b
kienquocv-oulu <kvu24@edu.oulu.fi>
2025-04-27 02:24:32 +0300

    Update alarm_clock.py

--- alarm_clock.py
+++ alarm_clock.py
@@ -1,56 +1,42 @@
 from machine import Pin, I2C, PWM
 import utime
 
+# LCD
 class LCD:
-    def __init__(self, rs, en, d4, d5, d6, d7):
-        self.rs = Pin(rs, Pin.OUT)
-        self.en = Pin(en, Pin.OUT)
-        self.data_pins = [Pin(d4, Pin.OUT), Pin(d5, Pin.OUT),
-                          Pin(d6, Pin.OUT), Pin(d7, Pin.OUT)]
+    def __init__(self, i2c, addr, rows, cols):
+        self.i2c = i2c
+        self.addr = addr
+        self.rows = rows
+        self.cols = cols
         self.init()
+    
+    def init(self):
+        for cmd in [0x33, 0x32, 0x28, 0x0C, 0x06, 0x01]:
+            self.cmd(cmd)
+            utime.sleep_ms(2)
+    
+    def cmd(self, cmd, mode=0):
+        high = mode | (cmd & 0xF0) | 0x08
+        low = mode | ((cmd << 4) & 0xF0) | 0x08
+        for val in [high | 4, high, low | 4, low]:
+            self.i2c.writeto(self.addr, bytes([val]))
+        
+        utime.sleep_us(50)
 
-    def pulse_enable(self):
-        self.en.low()
-        utime.sleep_us(1)
-        self.en.high()
-        utime.sleep_us(1)
-        self.en.low()
-        utime.sleep_us(100)
-
-    def send(self, data, rs_mode):
-        self.rs.value(rs_mode)
-        for i in range(4):
-            self.data_pins[i].value((data >> (4 + i)) & 0x01)
-        self.pulse_enable()
-        for i in range(4):
-            self.data_pins[i].value((data >> i) & 0x01)
-        self.pulse_enable()
-
-    def cmd(self, data):
-        self.send(data, 0)
-
-    def putstr(self, s):
-        for c in s:
-            self.send(ord(c), 1)
+    def move_to(self, col, row):
+        addr_begin = [0, 64, 20, 84] #I have no idea where these come from, but these works with 4x20 display
+        addr = 0x80 + addr_begin[row] + col
+        self.cmd(addr)
 
     def clear(self):
         self.cmd(0x01)
         utime.sleep_ms(2)
 
-    def move_to(self, col, row):
-        row_offsets = [0x00, 0x40]
-        self.cmd(0x80 | (col + row_offsets[row]))
+    def putstr(self, s):
+        for c in s:
+            self.cmd(ord(c), 1)
 
-    def init(self):
-        utime.sleep_ms(50)
-        self.send(0x33, 0)
-        self.send(0x32, 0)
-        self.send(0x28, 0)
-        self.send(0x0C, 0)
-        self.send(0x06, 0)
-        self.clear()
-
-
+#RTC
 class RTC:
     def __init__(self, i2c, addr=0x68):
         self.i2c = i2c
@@ -102,7 +88,7 @@ def get_distance_cm():
 
 
 i2c = I2C(0, scl=Pin(1), sda=Pin(0))
-lcd = LCD(rs=2, en=3, d4=4, d5=5, d6=6, d7=7)
+lcd = LCD(i2c, 0x27, 4, 20)
 rtc = RTC(i2c)
 
 
